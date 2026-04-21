4cc54de1fe87ac21b2f2d3e665979e06360ec1f3
yoyosasa512 <jx7ka4e@gmail.com>
2026-04-21 22:03:36 +0900

    Fix: Prioritize actual environment variables over .env contents

--- index.php
+++ index.php
@@ -171,7 +171,7 @@
                 list($name, $value) = explode('=', $line, 2);
                 $name = trim($name);
                 $value = trim($value);
-                if (!empty($name)) {
+                if (!empty($name) && getenv($name) === false) {
                     putenv(sprintf("%s=%s", $name, $value));
                     $_ENV[$name] = $value;
                     $_SERVER[$name] = $value;
